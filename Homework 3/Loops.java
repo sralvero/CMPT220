dc8f525fd18710f091c3df2d1ff4b215aa0b47c9
Sarah <sralvero@gmail.com>
2024-09-21 19:54:59 -0400

    finished loops challenge

--- Homework 3/Loops.java
+++ Homework 3/Loops.java
@@ -9,9 +9,33 @@ you need to explain why you picked your for loop conditional and what's doing th
 if you're confused reach out!
  */
 
-public class Main {
+import java.util.Scanner;
+
+public class Loops {
     public static void main(String[] args) {
 
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter a word: ");
+        String word = sc.nextLine();
+        int word_length = word.length();
+
+        for (int i = 0; i < word_length; i++) {
+            System.out.println(word.charAt(i));
+        }
+
+        /* This challenge starts with importing a scanner (of course) and getting a 
+        word from the user by saving their input to a variable. 
+        Then you can use .length() to find the length of that word, and save that to 
+        a new variable.
+        I set up the for loop with int i starting at 0 as normal, and then increasing 
+        until it equals word_length, at which point the for loop will stop.
+        I had to look up how to print a single character from the string, which Google 
+        told me could be done with the .charAt(index) command. 
+        In a string, the first index is 0 and the last index is word_length - 1 
+        and as the loop will run from i = 0 to i = word_length - 1, you can just print
+        word.charAt(i) in each iteration of the loop.
+         */
 
 
     }
--- Homework 3/LoopsDebug.java
+++ Homework 3/LoopsDebug.java
@@ -1,27 +1,37 @@
+import java.util.Scanner;
 
-public class Main {
+public class LoopsDebug {
     public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
 
 
         //P1: This one only prints 0-9, can you fix it so it prints 1-10?
         System.out.println("Problem 1");
         for (int i = 0; i < 10; i++){
-            System.out.println(i);
+            System.out.println(i + 1);
         }
 
         //P2: Ask the user for a number. Create a loop to find the factorial of it
         //(factorial = X!, X being the user input, Factorials are every digit before X multiplied together)
         System.out.println("Problem 2");
-        System.out.println("Enter a number and I will tell you the fatorial: ");
+        System.out.println("Enter a number and I will tell you the factorial: ");
         //here's a hint
-        for (int i = 0; i < ?; i++){
 
+        int num = sc.nextInt();
+        int factorial = 1; 
+        for (int i = 0; i < num; i++){
+            factorial = factorial * (i+1);
         }
+        System.out.println(factorial);
+        
 
         //P3: Ask the user for a number, and then add together every OTHER digit (starting from 1)
         System.out.println("Problem 3");
         System.out.println("Enter a number and I will tell you the sum of every other number: ");
         //No hint! what do you need to complete this task?
+        int var = sc.nextInt();
+
 
 
         //P4: Why does this loop never stop!
@@ -30,12 +40,19 @@ public class Main {
         boolean run = true;
         while (run == true){
             System.out.println("I printed once!");
+            run = false;
         }
+        // this loop doesn't stop because the run condition is always set to true 
+        // you can break the loop by setting run to false after the string prints once 
 
         //P5: Take a string from the user and print them the reverse!
         System.out.println("Problem 5");
         //hint
         String reverse = "";
+        
+        //e rm. use appends? 
+        for (int i = 0; i < 3; i++) {
 
+        }
     }
 }
