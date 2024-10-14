8afc0bd8cb5a0e7fe19add68f127b8cee0951661
Sarah <sralvero@gmail.com>
2024-10-14 01:53:44 -0400

    finished midterm
    unrandomized case values, wrote code for final six cases and last case scenario

--- Homework 4/midterm.java
+++ Homework 4/midterm.java
@@ -1,7 +1,8 @@
 // change folder at some point
 import java.util.List;
 import java.util.ArrayList;
-import java.util.Random;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.Scanner;
 import java.lang.Math; 
 
@@ -9,16 +10,12 @@ import java.lang.Math;
 public class midterm {
     public static void main(String[] args) { 
 
-        // instantiates classes
-        Random rand = new Random();
+        // instantiates scanner
         Scanner sc = new Scanner(System.in);
 
-        // creates a list with the values of the 26 cases, assigned randomly
+        // creates a list with the values of the 26 cases
         List<Integer> cases = new ArrayList<Integer>();
-        for (int i=0; i<26; i++) {
-            cases.add(rand.nextInt(100000));
-        } // will non-randomize this later im TIRED 
-        System.out.println("TEST values " + cases);
+        cases.addAll(Arrays.asList(1, 2, 5, 10, 25, 50, 75, 100, 200, 300, 400, 500, 750, 1000, 5000, 10000, 25000, 50000, 75000, 100000, 200000, 300000, 400000, 500000, 1000000, 1500000));
         System.out.println();
 
         // creates a list with the numbers of the 26 cases to be displayed to the user 
@@ -26,116 +23,337 @@ public class midterm {
         for (int i=0; i<26; i++) {
             caseNums.add(i+1);
         }
-        System.out.println("TEST numbers " + caseNums);
         System.out.println();
 
-        // intro text 
+        
         System.out.println("Welcome to Deal or No Deal!");
+        System.out.println("You have the opportunity to choose one of 26 cases with a random monetary value from the following list: ");
+        System.out.println(cases);
+        Collections.shuffle(cases); // randomizes order after displaying to the user
         System.out.println("Please choose a case by entering a number from 1 to 26.");
 
         // grabs user input and stores it to a variable, then removes the index with 
         // that value from the caseNums list 
         int userCase = sc.nextInt();
-        System.out.println("TEST choice" + userCase);
-        System.out.println("TEST index" + caseNums.indexOf(userCase));
         caseNums.remove(caseNums.indexOf(userCase));
 
         // saves the value of the user's original chosen case 
         int ogValue = cases.get(userCase - 1);
-        // System.out.println("TEST" + ogValue);
-        cases.remove(cases.indexOf(ogValue));
-        // System.out.println("TEST" + cases);
+        cases.remove(cases.indexOf(ogValue));        
 
-        
-        int casesToRemove = 6;
-        System.out.println();        
+        System.out.println();
 
-        for (int i = casesToRemove; i>0; i--) {
-            System.out.println("Pick " + casesToRemove + " cases to get rid of.");
+        // first round of discarding cases
+        for (int i = 6; i>0; i--) {
+            if (i == 1) {
+                System.out.println("Pick " + i + " case to get rid of.");
+            } else {
+                System.out.println("Pick " + i + " cases to get rid of.");
+            }
             System.out.println("Enter ONE number still present in the following list to discard the corresponding case.");
             System.out.println(caseNums); 
 
             // repeating the same code for the first selected case
             userCase = sc.nextInt();
-            System.out.println("TEST choice" + userCase);
-            System.out.println("TEST index" + caseNums.indexOf(userCase));
 
             // displays the value of each chosen case to the user 
             System.out.println("The value of case " + userCase + " was: $" + cases.get(caseNums.indexOf(userCase)));
-            System.out.println("TEST values " + cases);
             
             // updates both lists by removing the case value/number of the user's chosen case 
             cases.remove(caseNums.indexOf(userCase));
             caseNums.remove(caseNums.indexOf(userCase));
-
-            System.out.println("TEST values " + cases);
             
             System.out.println();
-
-            casesToRemove--;
-            System.out.println("TEST remaining cases: " + casesToRemove);
-            // what if we all just died 
         }
 
         System.out.println();
         
         // set up variables to calculate banker offer 
-        // I looked up what the banker offer formula was and it was apparently a root mean square, lol, lmao 
+        // banker offer formula is apparently a root mean square 
         double offer = 0; 
         for (int i=0; i<cases.size(); i++) {
             offer += Math.pow(cases.get(i), 2);
         }
-        offer = Math.sqrt(offer/cases.size());
+        offer = Math.ceil(Math.sqrt(offer/cases.size()));
 
         // this is the part where the banker makes an offer 
-        System.out.println("HELLO. i am the banker. i would like to make you an offer of $" + offer);
+        System.out.println("HELLO. i am the banker. i would like to make you an offer of $" + offer + ".");
         System.out.println("Type DEAL to accept. Type NO DEAL to keep playing.");
 
         String userChoice = sc.nextLine().toLowerCase();
 
-        // had to look up how to compare if a string does NOT equal another with the !.equals() command
+        // looked up how to compare if a string does NOT equal another with the !.equals() command
         while (!userChoice.equals("no deal") && !userChoice.equals("deal")) {
-            System.out.println(userChoice);
             System.out.println("Please enter DEAL or NO DEAL to continue.");
             userChoice = sc.nextLine().toLowerCase();
         }
 
-        if (userChoice.equals("no deal")) {
-            System.out.println("Thank you for playing Deal or No Deal! You have won $");
-            System.out.println("The case you originally picked contained $.");
-            System.exit(0);
+        if (userChoice.equals("deal")) {
+            System.out.println("Thank you for playing Deal or No Deal! You have won $" + offer + ".");
+            System.out.println("The case you originally picked contained $" + ogValue + ".");
+            System.exit(0); // ends game
+        } else if (userChoice.equals("no deal")) {
+            System.out.println("The game continues.");
+        }
+
+        // begins second round of removing cases. same code as first round but 1 less iteration 
+        for (int i=5; i>0; i--) {
+            if (i == 1) {
+                System.out.println("Pick " + i + " case to get rid of.");
+            } else {
+                System.out.println("Pick " + i + " cases to get rid of.");
+            }
+            System.out.println("Enter ONE number still present in the following list to discard the corresponding case.");
+            System.out.println(caseNums); 
+
+            userCase = sc.nextInt();
+
+            System.out.println("The value of case " + userCase + " was: $" + cases.get(caseNums.indexOf(userCase)));
+           
+            cases.remove(caseNums.indexOf(userCase));
+            caseNums.remove(caseNums.indexOf(userCase));
+            
+            System.out.println();
+        }
+
+        System.out.println();
+
+        // recalculating offer 
+        offer = 0;
+        for (int i=0; i<cases.size(); i++) {
+            offer += Math.pow(cases.get(i), 2);
+        }
+        offer = Math.ceil(Math.sqrt(offer/cases.size()));
+
+        System.out.println("HELLO AGAIN. i am the banker. i would like to make you an offer of $" + offer + ".");
+        System.out.println("Type DEAL to accept. Type NO DEAL to keep playing.");
+
+        userChoice = sc.nextLine().toLowerCase();
+
+        // once again checking if the user input a valid response 
+        while (!userChoice.equals("no deal") && !userChoice.equals("deal")) {
+            System.out.println("Please enter DEAL or NO DEAL to continue.");
+            userChoice = sc.nextLine().toLowerCase();
+        }
+
+        if (userChoice.equals("deal")) {
+            System.out.println("Thank you for playing Deal or No Deal! You have won $" + offer + ".");
+            System.out.println("The case you originally picked contained $" + ogValue + ".");
+            System.exit(0); // ends game
+        } else if (userChoice.equals("no deal")) {
+            System.out.println("The game continues.");
         }
 
 
+        // begins third round of removing cases 
+        for (int i=4; i>0; i--) {
+            if (i == 1) {
+                System.out.println("Pick " + i + " case to get rid of.");
+            } else {
+                System.out.println("Pick " + i + " cases to get rid of.");
+            }
+            System.out.println("Enter ONE number still present in the following list to discard the corresponding case.");
+            System.out.println(caseNums); 
+
+            userCase = sc.nextInt();
+
+            System.out.println("The value of case " + userCase + " was: $" + cases.get(caseNums.indexOf(userCase)));
+            
+            cases.remove(caseNums.indexOf(userCase));
+            caseNums.remove(caseNums.indexOf(userCase));
+            
+            System.out.println();
+        }
+
+        System.out.println();
+
+        // recalculating offer 
+        offer = 0;
+        for (int i=0; i<cases.size(); i++) {
+            offer += Math.pow(cases.get(i), 2);
+        }
+        offer = Math.ceil(Math.sqrt(offer/cases.size()));
+
+        System.out.println("HELLO AGAIN. i am the banker. i would like to make you an offer of $" + offer + ".");
+        System.out.println("Type DEAL to accept. Type NO DEAL to keep playing.");
+
+        userChoice = sc.nextLine().toLowerCase();
+
+        while (!userChoice.equals("no deal") && !userChoice.equals("deal")) {
+            System.out.println("Please enter DEAL or NO DEAL to continue.");
+            userChoice = sc.nextLine().toLowerCase();
+        }
+
+        if (userChoice.equals("deal")) {
+            System.out.println("Thank you for playing Deal or No Deal! You have won $" + offer + ".");
+            System.out.println("The case you originally picked contained $" + ogValue + ".");
+            System.exit(0); // ends game
+        } else if (userChoice.equals("no deal")) {
+            System.out.println("The game continues.");
+        }
 
 
-        /*
-         * 1. user picks one case out of 26. the monetary values are actually not 
-         * random LMFAO 
-         * 2. user picks 6 other cases to get rid of. as soon as a case is picked
-         * the value is revealed + it is removed from the list. text then displays 
-         * back to the user with pick 5 cases to get rid of etc 
-         * 3. once 6 cases have been removed banker makes an offer that is probably
-         * just the avg of the remaining cases. options are deal, no deal and 
-         * counteroffer (???)
-         * 4. if it declines your offer (or you choose no deal ig) you are then asked
-         * to choose 5 cases 
-         * 5. banker once again makes an offer. this time the value of the previous 
-         * offer will also be displayed. funnily there was no option to counteroffer
-         * 6. choose 4 cases
-         * 7. if you click deal it will tell you the value of the offer you just 
-         * accepted + have you view the value of the case you selected at the start
-         * 8. hilariously my case was worth $1 but my offer was $100k and I apparently
-         * "beat the banker"  
-         * 9. if you keep clicking no deal it will go to choose one case then deal/no
-         * deal. if you still click no deal it keeps making you choose one case 
-         * 10. when you get down to the last 2 it gives you an option to switch your 
-         * original case or not. once you make your choice it shows you what your case
-         * was first then the value of the other case 
-         * 11. I really dont know how they determine whether you win or not but lol
-         * + closing text
-         */
+        // begins fourth round of removing cases 
+        for (int i=3; i>0; i--) {
+            if (i == 1) {
+                System.out.println("Pick " + i + " case to get rid of.");
+            } else {
+                System.out.println("Pick " + i + " cases to get rid of.");
+            }
+            System.out.println("Enter ONE number still present in the following list to discard the corresponding case.");
+            System.out.println(caseNums); 
 
+            userCase = sc.nextInt();
+
+            System.out.println("The value of case " + userCase + " was: $" + cases.get(caseNums.indexOf(userCase)));
+            
+            cases.remove(caseNums.indexOf(userCase));
+            caseNums.remove(caseNums.indexOf(userCase));
+            
+            System.out.println();
+        }
+
+        System.out.println();
+
+        // recalculating offer 
+        offer = 0;
+        for (int i=0; i<cases.size(); i++) {
+            offer += Math.pow(cases.get(i), 2);
+        }
+        offer = Math.ceil(Math.sqrt(offer/cases.size()));
+
+        System.out.println("HELLO AGAIN. i am the banker. i would like to make you an offer of $" + offer + ".");
+        System.out.println("Type DEAL to accept. Type NO DEAL to keep playing.");
+
+        userChoice = sc.nextLine().toLowerCase();
+
+        while (!userChoice.equals("no deal") && !userChoice.equals("deal")) {
+            System.out.println("Please enter DEAL or NO DEAL to continue.");
+            userChoice = sc.nextLine().toLowerCase();
+        }
+
+        if (userChoice.equals("deal")) {
+            System.out.println("Thank you for playing Deal or No Deal! You have won $" + offer + ".");
+            System.out.println("The case you originally picked contained $" + ogValue + ".");
+            System.exit(0); // ends game
+        } else if (userChoice.equals("no deal")) {
+            System.out.println("The game continues.");
+        }
+
+
+        // begins fifth round of removing cases 
+        for (int i=2; i>0; i--) {
+            if (i == 1) {
+                System.out.println("Pick " + i + " case to get rid of.");
+            } else {
+                System.out.println("Pick " + i + " cases to get rid of.");
+            }
+            System.out.println("Enter ONE number still present in the following list to discard the corresponding case.");
+            System.out.println(caseNums); 
+
+            userCase = sc.nextInt();
+            
+            System.out.println("The value of case " + userCase + " was: $" + cases.get(caseNums.indexOf(userCase)));
+            System.out.println("TEST values " + cases);
+            
+            cases.remove(caseNums.indexOf(userCase));
+            caseNums.remove(caseNums.indexOf(userCase));
+            
+            System.out.println();
+        }
+
+        System.out.println();
+
+        // recalculating offer 
+        offer = 0;
+        for (int i=0; i<cases.size(); i++) {
+            offer += Math.pow(cases.get(i), 2);
+        }
+        offer = Math.ceil(Math.sqrt(offer/cases.size()));
+
+        System.out.println("HELLO AGAIN. i am the banker. i would like to make you an offer of $" + offer + ".");
+        System.out.println("Type DEAL to accept. Type NO DEAL to keep playing.");
+
+        userChoice = sc.nextLine().toLowerCase();
+
+        while (!userChoice.equals("no deal") && !userChoice.equals("deal")) {
+            System.out.println("Please enter DEAL or NO DEAL to continue.");
+            userChoice = sc.nextLine().toLowerCase();
+        }
+
+        if (userChoice.equals("deal")) {
+            System.out.println("Thank you for playing Deal or No Deal! You have won $" + offer + ".");
+            System.out.println("The case you originally picked contained $" + ogValue + ".");
+            System.exit(0); // ends game
+        } else if (userChoice.equals("no deal")) {
+            System.out.println("The game continues.");
+        }
+
+
+        // at this point you will keep choosing one case to discard --> banker offer 
+        // until you accept the offer or until 1 non-original case is left
+        while (cases.size() > 1) {
+            System.out.println("Pick 1 case to get rid of.");
+            System.out.println("Enter ONE number still present in the following list to discard the corresponding case.");
+            System.out.println(caseNums); 
+    
+            userCase = sc.nextInt();
+
+            System.out.println("The value of case " + userCase + " was: $" + cases.get(caseNums.indexOf(userCase)));
+            
+            cases.remove(caseNums.indexOf(userCase));
+            caseNums.remove(caseNums.indexOf(userCase));
+
+            System.out.println();
+
+            offer = 0;
+            for (int i=0; i<cases.size(); i++) {
+                offer += Math.pow(cases.get(i), 2);
+            }
+            offer = Math.ceil(Math.sqrt(offer/cases.size()));
+
+            if (cases.size() != 1) {
+                System.out.println("HELLO AGAIN. i am the banker. i would like to make you an offer of $" + offer + ".");
+                System.out.println("Type DEAL to accept. Type NO DEAL to keep playing.");
+
+                userChoice = sc.nextLine().toLowerCase();
+
+                while (!userChoice.equals("no deal") && !userChoice.equals("deal")) {
+                    System.out.println("Please enter DEAL or NO DEAL to continue.");
+                    userChoice = sc.nextLine().toLowerCase();
+                }
+
+                if (userChoice.equals("deal")) {
+                    System.out.println("Thank you for playing Deal or No Deal! You have won $" + offer + ".");
+                    System.out.println("The case you originally picked contained $" + ogValue + ".");
+                    System.exit(0); // ends game
+                } else if (userChoice.equals("no deal")) {
+                    System.out.println("The game continues.");
+                }
+            }
+        }
+
+        System.out.println();
+        System.out.println("The last remaining case is " + caseNums + ".");
+        System.out.println("Would you like to keep your original case or switch it?");
+        System.out.println("Type DEAL to switch to the other case. Type NO DEAL to keep your original case.");
+
+        userChoice = sc.nextLine().toLowerCase();
+
+        while (!userChoice.equals("no deal") && !userChoice.equals("deal")) {
+            System.out.println("Please enter DEAL or NO DEAL to continue.");
+            userChoice = sc.nextLine().toLowerCase();
+        }
+
+        if (userChoice.equals("deal")) {
+            System.out.println("You have switched to case " + caseNums.get(0) + " and won $" + cases.get(0) + ".");
+            System.out.println("The case you originally picked contained $" + ogValue + ".");
+        } else if (userChoice.equals("no deal")) {
+            System.out.println("You have kept your original case and won $" + ogValue + ".");
+            System.out.println("The value of case " + caseNums.get(0) + " was $" + cases.get(0) + ".");
+        }
+        System.out.println("Thank you for playing Deal or No Deal.");
+        System.out.println();
 
     }
 }
--- Homework 4/test.java
+++ Homework 4/test.java
@@ -1,9 +1,23 @@
 import java.util.Scanner;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.lang.Math;
+import java.util.Collections;
 
 public class test {
     public static void main(String[] args) { 
+        List<Integer> cases = new ArrayList<Integer>();
+        cases.addAll(Arrays.asList(1, 2, 5, 10, 25, 50, 75, 100, 200, 300, 400, 500, 750, 1000, 5000, 10000, 25000, 50000, 75000, 100000, 200000, 300000, 400000, 500000, 1000000, 1500000));
+        Collections.shuffle(cases);
+        System.out.println("test " + cases);
 
-        Scanner sc = new Scanner(System.in);
+
+
+
+
+
+        /* Scanner sc = new Scanner(System.in);
         System.out.println("Please enter something lol");
         String userChoice = sc.nextLine().toLowerCase();
 
@@ -17,8 +31,8 @@ public class test {
         if (userChoice.equals("no deal")) {
             System.out.println("Thank you for playing Deal or No Deal! You have won $");
             System.out.println("The case you originally picked contained $.");
-            System.exit(0);
-        }
+            System.exit(0); 
+        } */
 
     }
 
